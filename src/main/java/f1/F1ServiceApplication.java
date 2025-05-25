0e334d8fa60f87d797a0fdfbba6f694c43ae6551
Marcos <marcosdeixx@gmail.com>
2025-05-25 00:36:28 -0300

    feat: Criando estrutura base do projeto com Spring Boot v0.0.1

---
+++ src/main/java/f1/F1ServiceApplication.java
@@ -0,0 +1,13 @@
+package f1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f1/F1ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package f1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
